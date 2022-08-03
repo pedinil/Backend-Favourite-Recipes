6c99f9858299ba34c92c1c8dd2960144c9957864
Pedram Ezzati <pedram.ezzati@ceitasl.com>
2022-08-03 16:23:14 +0200

    initiating the pom.xml dependency

---
+++ src/main/java/com/interview/backendfavouriterecipes/BackendFavouriteRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.interview.backendfavouriterecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFavouriteRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendFavouriteRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/interview/backendfavouriterecipes/BackendFavouriteRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.backendfavouriterecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFavouriteRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
